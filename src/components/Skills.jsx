c4a3c965b48d1b731657bd4c0b5faee9372ad3e5
Douglas <douglasl.batista@hotmail.com>
2023-01-27 15:59:22 -0300

    version 1.5

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,8 +2,103 @@ function Skills(){
     return(
         <div className="skills">
             <h1>Habilidades</h1>
-            
-        </div>
+                <div class="container">
+                 <div class="card">
+                 <div class="face face1">
+                  <div class="content">
+                  <i class="fab fa-windows"></i>
+                  <h3>React</h3>
+                 </div>
+                  </div>
+                  <div class="face face2">
+                  <div class="content">
+                  <p> React.</p>
+                  <a href="#" type="button">Ver mais</a>
+              </div>
+             </div>
+             </div>
+              </div>
+              <div class="container">
+                 <div class="card">
+                 <div class="face face1">
+                  <div class="content">
+                  <i class="fab fa-windows"></i>
+                  <h3>Angular</h3>
+                 </div>
+                  </div>
+                  <div class="face face2">
+                  <div class="content">
+                  <p> Angular.</p>
+                  <a href="#" type="button">Ver mais</a>
+              </div>
+             </div>
+             </div>
+              </div>             
+              <div class="container">
+                 <div class="card">
+                 <div class="face face1">
+                  <div class="content">
+                  <i class="fab fa-windows"></i>
+                  <h3>JavaScript</h3>
+                 </div>
+                  </div>
+                  <div class="face face2">
+                  <div class="content">
+                  <p> JavaScript.</p>
+                  <a href="#" type="button">Ver mais</a>
+              </div>
+             </div>
+             </div>
+              </div>
+              <div class="container">
+                 <div class="card">
+                 <div class="face face1">
+                  <div class="content">
+                  <i class="fab fa-windows"></i>
+                  <h3>TypeScript</h3>
+                 </div>
+                  </div>
+                  <div class="face face2">
+                  <div class="content">
+                  <p> TypeScript.</p>
+                  <a href="#" type="button">Ver mais</a>
+              </div>
+             </div>
+             </div>
+              </div>
+              <div class="container">
+                 <div class="card">
+                 <div class="face face1">
+                  <div class="content">
+                  <i class="fab fa-windows"></i>
+                  <h3>HTML5</h3>
+                 </div>
+                  </div>
+                  <div class="face face2">
+                  <div class="content">
+                  <p> HTML5.</p>
+                  <a href="#" type="button">Ver mais</a>
+              </div>
+             </div>
+             </div>
+              </div>
+              <div class="container">
+                 <div class="card">
+                 <div class="face face1">
+                  <div class="content">
+                  <i class="fab fa-windows"></i>
+                  <h3>CSS3</h3>
+                 </div>
+                  </div>
+                  <div class="face face2">
+                  <div class="content">
+                  <p> CSS3.</p>
+                  <a href="#" type="button">Ver mais</a>
+              </div>
+             </div>
+             </div>
+              </div>
+             </div>
     )
 }
 export default Skills
